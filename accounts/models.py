7992c68405819653dff991fd5763a5f01303b43a
FursykIvan <ivan.fursyk@gmail.com>
2024-12-03 15:43:48 +0200

    add IndexView to auction app

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     groups = models.ManyToManyField(
         "auth.Group",
         related_name="accounts_user_set",
--- auction/views.py
+++ auction/views.py
@@ -1,3 +1,30 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.utils.timezone import now
+from auction.models import Auction, Watchlist
+from itertools import chain
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        auctions = Auction.objects.filter(end_time__gte=now()).order_by('start_time')
+        context['auctions'] = auctions
+
+        if self.request.user.is_authenticated:
+            user = self.request.user
+            watchlist_items = Watchlist.objects.filter(user_id=user)
+            watchlist = Auction.objects.none()
+            for item in watchlist_items:
+                auctions_for_watchlist = Auction.objects.filter(id=item.auction_id.id)
+                watchlist = list(chain(watchlist, auctions_for_watchlist))
+
+            context['watchlist'] = watchlist
+            context['balance'] = user.balance
+        return context
