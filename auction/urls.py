72c2451f584066218d37b52228ca877f655f5545
FursykIvan <ivan.fursyk@gmail.com>
2024-12-06 00:34:27 +0200

    Register models in admin: User, Category, AuctionListing, Bid, Comment Add URL patterns for views: IndexView, AllListingsView, login_view, logout_view, register, createlisting, details, categories, filter_by_category, comment, place_bid, auction_ending, watchlist, watch
    - Registered models with the Django admin interface to manage data through the admin panel.
    - Added URL patterns for various views to handle routing in the application.

--- auction/urls.py
+++ auction/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
+from . import views
 from .views import IndexView, AllListingsView
+
+app_name = "auction"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("all/", AllListingsView.as_view(), name="all_listings"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("createListing", views.createlisting, name="createListing"),
+    path("details/<int:id>", views.details, name="details"),
+    path("categories", views.categories, name="categories"),
+    path("filter/<str:name>", views.filter_by_category, name="filter"),
+    path("comment/<int:id>", views.comment, name="comment"),
+    path("bid/<int:id>", views.place_bid, name="bid"),
+    path("end/<int:item_id>", views.auction_ending, name="end"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("watch", views.watch, name="watch")
 ]
